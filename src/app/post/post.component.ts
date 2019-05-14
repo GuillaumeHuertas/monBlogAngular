0f707cbfd0b617f568e6a8663ddcdc3afdfbda99
Guillaume Huertas <guillaumehuertas@free.fr>
2019-05-14 12:14:37 +0200

    Bouton j'aime et j'aime pas fonctionnels

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  postTitle : string = 'Le premier jour'; 
-  postBody: string = 'Voilà le premier jour arrivé'; 
+  @Input() postTitle: String;
+  @Input() postBody: String; 
+  
   postPoint: number = 0; 
 
   constructor() { }
